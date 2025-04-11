a32fbec6a630330e1e580d4d0db87b78f963a214
Matthew Parker <mparker@mpipz.mpg.de>
2025-04-11 12:29:36 +0200

    docstrings for bam.py

--- snco/bam.py
+++ snco/bam.py
@@ -22,10 +22,26 @@ DEFAULT_EXCLUDE_CONTIGS = set([
 @dataclass
 class IntervalMarkerCounts:
 
-    '''
-    dataclass to store deduplicated count information
-    for a single bin/interval
-    '''
+    """
+    Dataclass to store deduplicated count information for a single bin/interval.
+
+    Attributes
+    ----------
+    chrom : str
+        Chromosome name.
+    bin_idx : int
+        Index of the bin within the chromosome.
+    counts : dict
+        A dictionary storing counts for each cell barcode (CB) and haplotype (hap).
+
+    Methods
+    -------
+    deep_items()
+        Yields all items in counts (cell barcode, haplotype, value).
+    new_like(other)
+        Returns a new instance of IntervalMarkerCounts with the same chromosome and 
+        bin index as `other`.
+    """
 
     chrom: str
     bin_idx: int
@@ -40,21 +56,60 @@ class IntervalMarkerCounts:
         self.counts[index] = Counter(val)
 
     def deep_items(self):
+        """
+        Yields all items in counts (cell barcode, haplotype, value).
+
+        Yields
+        ------
+        tuple
+            (cell barcode, haplotype, count value).
+        """
         for cb, hap_counts in self.counts.items():
             for hap, val in hap_counts.items():
                 yield cb, hap, val
 
     @classmethod
     def new_like(cls, other):
+        """
+        Returns a new instance of IntervalMarkerCounts with the same chromosome 
+        and bin index as `other`.
+
+        Parameters
+        ----------
+        other : IntervalMarkerCounts
+            The instance from which to copy chromosome and bin index.
+
+        Returns
+        -------
+        IntervalMarkerCounts
+            A new IntervalMarkerCounts instance with the same chrom and bin_idx as `other`.
+        """
         return cls(other.chrom, other.bin_idx)
 
 
 class IntervalUMICounts:
 
-    '''
-    class to store count information for a single bin/interval
-    prior to UMI deduplication
-    '''
+    """
+    Class to store count information for a single bin/interval prior to UMI deduplication.
+
+    Attributes
+    ----------
+    chrom : str
+        Chromosome name.
+    bin_idx : int
+        Index of the bin within the chromosome.
+    umi_collapse_method : str
+        The UMI collapse method used ('directional', 'exact' or None).
+    hap_tag_type : str
+        The haplotype tag type used ('star_diploid', 'multi_haplotype').
+    has_umi : bool
+        Whether UMI collapse is applied.
+
+    Methods
+    -------
+    collapse()
+        Deduplicates UMIs and returns an IntervalMarkerCounts object with collapsed counts.
+    """
 
     def __init__(self, chrom, bin_idx, umi_collapse_method, hap_tag_type):
         self.chrom = chrom
@@ -92,14 +147,40 @@ class IntervalUMICounts:
     def __iter__(self):
         return iter(self._counts)
 
-    def hap_collapse_star_diploid(self, hap_counts):
+    def _hap_collapse_star_diploid(self, hap_counts):
+        """
+        Collapses haplotype counts in star-diploid format.
+
+        Parameters
+        ----------
+        hap_counts : dict
+            A dictionary of haplotype counts.
+
+        Returns
+        -------
+        str or None
+            The collapsed haplotype, or None if it cannot be collapsed.
+        """
         hap = list(hap_counts.keys())
         if len(hap) > 1:
             return None
         else:
             return hap[0]
 
-    def hap_collapse_multi_haplotype(self, hap_counts):
+    def _hap_collapse_multi_haplotype(self, hap_counts):
+        """
+        Collapses haplotype counts for multi-haplotype format.
+
+        Parameters
+        ----------
+        hap_counts : dict
+            A dictionary of haplotype counts.
+
+        Returns
+        -------
+        frozenset or None
+            The supported haplotypes as a frozenset, or None if it cannot be collapsed.
+        """
         hap = reduce(frozenset.intersection, hap_counts.keys())
         if not len(hap):
             return None
@@ -107,10 +188,14 @@ class IntervalUMICounts:
             return hap
 
     def collapse(self):
-        '''
-        Deduplicate UMIs in the interval using one of three methods and
-        return as an IntervalMarkerCounts object
-        '''
+        """
+        Deduplicates UMIs and returns an IntervalMarkerCounts object with collapsed counts.
+
+        Returns
+        -------
+        IntervalMarkerCounts
+            A new IntervalMarkerCounts object containing the collapsed counts.
+        """
 
         if self.umi_collapse_method is None:
             umi_eval_method = methodcaller('total')
@@ -118,9 +203,9 @@ class IntervalUMICounts:
             umi_eval_method = lambda hap_counts: 1
 
         if self.hap_tag_type == "star_diploid":
-            hap_eval_method = self.hap_collapse_star_diploid
+            hap_eval_method = self._hap_collapse_star_diploid
         else:
-            hap_eval_method = self.hap_collapse_multi_haplotype
+            hap_eval_method = self._hap_collapse_multi_haplotype
 
         def umi_eval(umi_hap_counts):
             total = Counter()
@@ -141,6 +226,28 @@ class IntervalUMICounts:
 
 
 def get_ha_samples(bam_fn):
+    """
+    Retrieves haplotype samples from a BAM file.
+
+    This function attempts to extract haplotype sample information from the header of the BAM file
+    in multiple ways (using the 'ha' field in the HD header, or extracting from the 'ha_flag_accessions' 
+    comment or RG fields).
+
+    Parameters
+    ----------
+    bam_fn : str
+        Path to the BAM file.
+
+    Returns
+    -------
+    list of str
+        A sorted list of haplotype sample identifiers.
+
+    Raises
+    ------
+    ValueError
+        If no haplotype information is found in the BAM file header.
+    """
     with pysam.AlignmentFile(bam_fn) as bam:
         try:
             return set(bam.header['HD']['ha'].split(','))
@@ -159,10 +266,41 @@ def get_ha_samples(bam_fn):
 
 
 class BAMHaplotypeIntervalReader:
+    """
+    File wrapper class for reading aggregated bin counts for each cell barcode/haplotype.
 
-    '''
-    File wrapper class for reading aggregated bin counts for each cell barcode/haplotype
-    '''
+    This class facilitates the reading of a BAM file, specifically extracting and aggregating
+    count information for each interval (bin) within the genome for a given cell barcode (CB)
+    and haplotype.
+
+    Attributes
+    ----------
+    bin_size : int
+        The size of each bin used for aggregation (default: 25,000).
+    cb_tag : str
+        The tag used to store the cell barcode (default: 'CB').
+    umi_tag : str
+        The tag used to store the UMI (default: 'UB').
+    hap_tag : str
+        The tag used to store the haplotype (default: 'ha').
+    hap_tag_type : str
+        The haplotype tag type ('star_diploid' or 'multi_haplotype').
+    haplotypes : frozenset
+        A set of allowed haplotypes.
+    cb_whitelist : object
+        An optional whitelist for cell barcodes.
+    umi_collapse_method : str
+        The method for collapsing UMIs ('directional', None, etc.).
+    exclude_contigs : set
+        Set of contig names to exclude (default: mitochondrial, plastid).
+
+    Methods
+    -------
+    fetch_interval_counts(chrom, bin_idx)
+        Aggregates alignments into an IntervalMarkerCounts object for the specified bin and chromosome.
+    close()
+        Closes the BAM file.
+    """
 
     def __init__(self, bam_fn, *,
                  bin_size=25_000,
@@ -174,11 +312,41 @@ class BAMHaplotypeIntervalReader:
                  cb_whitelist=None,
                  umi_collapse_method='directional',
                  exclude_contigs=None):
+        """
+        File wrapper class for reading aggregated bin counts for each cell barcode/haplotype.
+    
+        This class facilitates the reading of a BAM file, specifically extracting and aggregating
+        count information for each interval (bin) within the genome for a given cell barcode (CB)
+        and haplotype.
+
+        Parameters
+        ----------
+        bam_fn : str
+            Path to the BAM file.
+        bin_size : int, optional
+            The size of each bin used for aggregation (default is 25,000).
+        cb_tag : str, optional
+            The tag used for the cell barcode (default is 'CB').
+        umi_tag : str, optional
+            The tag used for the UMI (default is 'UB').
+        hap_tag : str, optional
+            The tag used for the haplotype (default is 'ha').
+        hap_tag_type : str, optional
+            The haplotype tag type ('star_diploid' or 'multi_haplotype', default is 'star_diploid').
+        allowed_haplotypes : list of str, optional
+            List of allowed haplotypes. If None, the haplotypes are extracted from the BAM header.
+        cb_whitelist : snco.barcodes.CellBarcodeWhitelist, optional
+            A whitelist for cell barcodes, supports 1MM fuzzy matching.
+        umi_collapse_method : str, optional
+            The method for collapsing UMIs ('directional', 'exact', None).
+        exclude_contigs : set, optional
+            A set of contig names to exclude from processing (default is mitochondrial and plastid contigs).
+        """
         self._bam_fn = bam_fn
         self.bin_size = bin_size
-        self._cb_tag = cb_tag
-        self._umi_tag = umi_tag
-        self._hap_tag = hap_tag
+        self.cb_tag = cb_tag
+        self.umi_tag = umi_tag
+        self.hap_tag = hap_tag
         self.hap_tag_type = hap_tag_type
         if allowed_haplotypes is not None:
             allowed_haplotypes = frozenset(allowed_haplotypes)
@@ -204,11 +372,32 @@ class BAMHaplotypeIntervalReader:
             self.haplotypes = get_ha_samples(self._bam_fn)
 
     def fetch_interval_counts(self, chrom, bin_idx):
-        '''
-        for a specific bin index (positions determined by bin_size)
-        read bam file and aggregate alignments into IntervalMarkerCounts
-        object.
-        '''
+        """
+        Aggregates alignments into an IntervalMarkerCounts object for the specified bin and chromosome.
+
+        This function fetches alignments from the BAM file for a given chromosome and bin index,
+        and aggregates the alignments into an IntervalMarkerCounts object based on the cell barcode,
+        UMI, and haplotype.
+
+        Parameters
+        ----------
+        chrom : str
+            The chromosome name.
+        bin_idx : int
+            The index of the bin within the chromosome.
+
+        Returns
+        -------
+        IntervalMarkerCounts
+            An object containing the aggregated counts for the specified bin.
+
+        Raises
+        ------
+        IOError
+            If the BAM records do not contain the expected tags (e.g., CB, UB, ha).
+        ValueError
+            If the haplotype information in the BAM file does not match the expected format.
+        """
         assert bin_idx < self.nbins[chrom]
         bin_start = self.bin_size * bin_idx
         bin_end = bin_start + self.bin_size - 1
@@ -227,10 +416,10 @@ class BAMHaplotypeIntervalReader:
                     continue
 
             try:
-                hap = aln.get_tag(self._hap_tag)
+                hap = aln.get_tag(self.hap_tag)
             except KeyError as exc:
                 raise IOError(
-                    f'bam records do not all have the haplotype tag "{self._hap_tag}"'
+                    f'bam records do not all have the haplotype tag "{self.hap_tag}"'
                 ) from exc
 
             if self.hap_tag_type == 'star_diploid':
@@ -258,12 +447,12 @@ class BAMHaplotypeIntervalReader:
                 continue
 
             try:
-                cb = aln.get_tag(self._cb_tag)
+                cb = aln.get_tag(self.cb_tag)
                 if cb == '-':
                     cb = None
             except KeyError as exc:
                 raise IOError(
-                    f'bam records do not all have the cell barcode tag "{self._cb_tag}"'
+                    f'bam records do not all have the cell barcode tag "{self.cb_tag}"'
                 ) from exc
 
             # finally filter for barcodes in the whitelist
@@ -272,12 +461,12 @@ class BAMHaplotypeIntervalReader:
                 cb = self.cb_whitelist.correct(cb)
 
             if cb is not None:
-                if self._umi_tag is not None:
+                if self.umi_tag is not None:
                     try:
-                        umi = aln.get_tag(self._umi_tag)
+                        umi = aln.get_tag(self.umi_tag)
                     except KeyError as exc:
                         raise IOError(
-                            f'bam records do not all have the UMI tag "{self._umi_tag}". '
+                            f'bam records do not all have the UMI tag "{self.umi_tag}". '
                             'Maybe you meant to run without UMI deduplication?'
                         ) from exc
                 else:
