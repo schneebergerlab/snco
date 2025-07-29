4fba413a09c98696c8d56a574daf2e4786c99ba7
mparker <mparker@mpipz.mpg.de>
2025-07-29 18:13:26 +0200

    add decoy functionality to collapse script. Untested

--- scripts/collapse_ha_specific_alns.py
+++ scripts/collapse_ha_specific_alns.py
@@ -9,6 +9,9 @@ import click
 
 
 def test_close_alignment_positions(chroms, positions, pos_tol=10):
+    assert len(chroms) == len(positions)
+    if not len(chroms):
+        return True
     same_chrom = all([c == chroms[0] for c in chroms])
     if not same_chrom:
         return False
@@ -31,13 +34,26 @@ def group_by_queryname(bam_iter):
         yield group
 
 
-def aln_collapser(bam_bundle_iter, pos_tol=10):
+def strip_mapping_info(aln):
+    aln.is_unmapped = True
+    aln.reference_id = -1
+    aln.reference_start = -1
+    aln.mapping_quality = 0
+    aln.cigar = None
+    aln.next_reference_id = -1
+    aln.next_reference_start = -1
+    return aln
+
+
+def aln_collapser(bam_bundle_iter, decoy_haplotypes, pos_tol=10):
     for bundle in bam_bundle_iter:
-        high_score = 0
+        high_score = -float('inf')
         representative_aln = None
         representative_mate = None
+        repr_is_decoy = None
         is_paired = False
-        ha_flag = []
+        ha_tag = None
+        dh_tag = None
         hs_chroms = []
         hs_positions = []
         for aln in bundle:
@@ -49,45 +65,70 @@ def aln_collapser(bam_bundle_iter, pos_tol=10):
                 # skip bundles with multimappers
                 break
             score = aln.get_tag('AS')
+            rg_tag = aln.get_tag('RG')
+            is_decoy = rg_tag in decoy_haplotypes
             if score > high_score:
                 if not aln.is_paired or aln.is_read1:
                     representative_aln = aln
+                    representative_mate = None
+                    hs_chroms = [aln.reference_name, ] if not is_decoy else []
+                    hs_positions = [aln.reference_start, ] if not is_decoy else []
                 else:
                     representative_mate = aln
+                    representative_aln = None
                     is_paired = True
-                ha_flag = [aln.get_tag('RG'),]
+                    hs_chroms = []
+                    hs_positions = []
+                if not is_decoy:
+                    ha_tag = {rg_tag,}
+                    dh_tag = set()
+                else:
+                    ha_tag = set()
+                    dh_tag = {rg_tag,}
+                repr_is_decoy = is_decoy
                 high_score = score
-                hs_chroms = [aln.reference_name, ]
-                hs_positions = [aln.reference_start, ]
             elif score == high_score:
                 if not aln.is_paired or aln.is_read1:
-                    if representative_aln is None:
+                    if representative_aln is None or repr_is_decoy:
                         representative_aln = aln
-                    ha_flag.append(aln.get_tag('RG'))
+                        repr_is_decoy = is_decoy
                     hs_chroms.append(aln.reference_name)
                     hs_positions.append(aln.reference_start)
                 else:
-                    if representative_mate is None:
+                    if representative_mate is None or repr_is_decoy:
                         representative_mate = aln
+                        repr_is_decoy = is_decoy
                     is_paired = True
+                if not is_decoy:
+                    ha_tag.add(rg_tag)
+                else:
+                    dh_tag.add(rg_tag)
             else:
                 continue
         else:
             if representative_aln is not None:
-                ha_flag.sort()
-                ha_tag = ','.join(ha_flag)
+                ha_tag = sorted(ha_tag)
+                ha_tag = ','.join(ha_tag)
+                dh_tag = sorted(dh_tag)
+                dh_tag = ','.join(dh_tag)
+                if repr_is_decoy:
+                    representative_aln = strip_mapping_info(representative_aln)
+                    if is_paired:
+                        representative_mate = strip_mapping_info(representative_mate)
                 if test_close_alignment_positions(hs_chroms, hs_positions, pos_tol):
                     representative_aln.set_tag('ha', ha_tag)
+                    representative_aln.set_tag('dh', dh_tag)
                     representative_aln.set_tag('RG', None)
                     yield representative_aln
                     if is_paired:
                         representative_mate.set_tag('ha', ha_tag)
+                        representative_mate.set_tag('dh', dh_tag)
                         representative_mate.set_tag('RG', None)
                         yield representative_mate
 
 
 @contextmanager
-def collapse_bam_alignments(bam_fn, position_tolerance=10):
+def collapse_bam_alignments(bam_fn, decoy_haplotypes, position_tolerance=10):
     bam = pysam.AlignmentFile(bam_fn, mode='rb')
 
     header = bam.header.to_dict()
@@ -107,6 +148,7 @@ def collapse_bam_alignments(bam_fn, position_tolerance=10):
 
     bam_iter = aln_collapser(
         group_by_queryname(bam.fetch(until_eof=True)),
+        decoy_haplotypes,
         position_tolerance
     )
 
@@ -119,9 +161,11 @@ def collapse_bam_alignments(bam_fn, position_tolerance=10):
 @click.command()
 @click.argument('input-bam', nargs=1, required=True)
 @click.option('-o', '--output-bam', nargs=1, required=True)
+@click.option('--decoy-haplotypes', required=False, help='Comma-separated list of decoy RG IDs', default=None)
 @click.option('--position-tolerance', required=False, default=10)
-def main(input_bam, output_bam, position_tolerance):
-    with collapse_bam_alignments(input_bam, position_tolerance) as (header, bam_iter):
+def main(input_bam, output_bam, decoy_haplotypes, position_tolerance):
+    decoy_haplotypes = set(decoy_haplotypes.split(',')) if decoy_haplotypes else set()
+    with collapse_bam_alignments(input_bam, decoy_haplotypes, position_tolerance) as (header, bam_iter):
         with pysam.AlignmentFile(output_bam, 'wb', header=header) as output_bam:
             for aln in bam_iter:
                 output_bam.write(aln)
