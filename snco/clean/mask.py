fix bed masking

snco/clean/mask.py
@@ -215,8 +215,9 @@ def mask_regions_bed(co_markers, mask_bed_fn):
     bs = co_markers.bin_size
     for chrom, invs in mask_invs.groupby('chrom'):
         start_bins = np.floor(invs.start // bs).astype(int)
-        end_bins = np.ceil(invs.end // bs).astype(int)        
-        for cb, m in co_markers_m.iter_chrom(chrom):
+        end_bins = np.ceil(invs.end // bs).astype(int)
+        
+        for m in co_markers_m[:, chrom].values():
             for s, e in zip(start_bins, end_bins):
                 m[s: e] = 0
     return co_markers_m