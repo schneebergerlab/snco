ed07d93cf9505937196e244e32af97d9bf81eaf2
mparker <mparker@mpipz.mpg.de>
2025-12-18 11:34:45 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='snco',
-    version='0.5.1',
+    version='0.6.0',
     description=(
         'crossover mapping for single cell/nucleus sequencing data'
     ),
